459cc6d7ab20b6d3ec81c386a9a28296b517c97e
hamdhanvkr <hamdhanvkr@gmail.com>
2024-12-03 21:06:41 +0530

    env file and backend setup

---
+++ server.jsx
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: process.env.FRONTEND_URL }));
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Failed to connect to MongoDB:', err));
+
+// Start Server
+const port = process.env.PORT || 5001;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
