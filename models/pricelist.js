71671b0c48121d5f7baeca6eee77ca6db85e407a
hamdhanvkr <hamdhanvkr@gmail.com>
2024-12-06 15:54:33 +0530

    first page

---
+++ models/pricelist.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const PriceSchema = new mongoose.Schema({
+    s_no :  Number,
+    p_id :  Number,
+    pt_id :  Number,
+    price : Number,
+    unit: {
+        type: String,
+        default: 0
+    }   
+})
+PriceSchema.plugin(AutoIncrement, { inc_field: 's_no' });
+
+const PriceModel = mongoose.model("pricelist", PriceSchema)
+module.exports = PriceModel
--- server.jsx
+++ server.jsx
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const PriceModel = require('./models/pricelist');
 
 const app = express();
 
