f45f4d81fac2f3b34a41c71f3f1fe118cd66b917
sirajhanifa <sirajhanifa786@gmail.com>
2024-12-09 20:38:23 +0530

    modify

--- server.jsx
+++ server.jsx
@@ -65,17 +65,20 @@ app.post('/subcategory', async (req, res) =>
 	try 
 	{
 		const { product_name } = req.body;
-		console.log(product_name)
 		const product = await products.findOne({ product_name });
 
 		const productId = product.product_id;
 
-		const categories = await category.find({ product_id: productId }, 'sub_category' )
+		const categories = await category.find({ product_id: productId }, 'category' )
 
-		const subCategories = [...new Set(categories.map((category) => category.sub_category))]
-		const type = await category.find({sub_categories: subCategories})
-		console.log(type)
-		console.log(categories)
+
+
+		const subCategories = [...new Set(categories.map((category) => category.category))]
+		// console.log(subCategories)
+
+		// const type = await category.find({sub_categories: subCategories})
+		// console.log(type)
+		// console.log(categories)
 
 		res.status(200).json({ sub_categories: subCategories });
 	} 
@@ -86,3 +89,27 @@ app.post('/subcategory', async (req, res) =>
 })
 
 //----------------------------------------------------------------------------------------------
+
+// Route to Fetch Type based on a sub category
+
+app.post('/type', async (req, res) => 
+	{
+		try 
+		{
+			const { sub_category } = req.body;
+			
+	
+			const type = await category.find({ category: sub_category }, 'type' )
+
+			// console.log(type);
+	
+			res.status(200).json({ type: type });
+		} 
+		catch (error) {
+			console.error('Error in Subcategory Route:', error);
+			res.status(500).json({ error: 'Internal Server Error' });
+		}
+	})
+	
+
+
