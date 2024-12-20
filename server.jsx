cb2915e54a09cabbb2b489585732358b69ba32ff
hamdhanvkr <hamdhanvkr@gmail.com>
2024-12-20 17:14:30 +0530

    measure

--- server.jsx
+++ server.jsx
@@ -34,7 +34,7 @@ mongoose
 
 // Start the 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
